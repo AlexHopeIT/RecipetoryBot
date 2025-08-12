b6e978930d1ebac203fbee889478621fd8982536
Alexey Schcennikov <hopefortomorrow@bk.ru>
2025-08-12 03:44:46 +0300

    Добавлен поиск по ингредиентам

--- handlers/states.py
+++ handlers/states.py
@@ -4,3 +4,8 @@ from aiogram.fsm.state import State, StatesGroup
 class FindRecipeState(StatesGroup):
     waiting_for_name = State()
     waiting_for_choice = State()
+
+
+class ByIngredientsState(StatesGroup):
+    waiting_for_ingredients = State()
+    waiting_for_choice = State()
--- keyboards/inline.py
+++ keyboards/inline.py
@@ -3,10 +3,21 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 def main_menu_keyboard():
     '''Создание инлайн-клавы главного меню'''
-    find_button = InlineKeyboardButton(text='🔍 Найти рецепт', callback_data='find_recipe_inline')
-    random_button = InlineKeyboardButton(text='📜 Случайный рецепт', callback_data='random_recipe_inline')
+    find_button = InlineKeyboardButton(
+        text='🔍 Найти рецепт',
+        callback_data='find_recipe_inline'
+        )
+    random_button = InlineKeyboardButton(
+        text='📜 Случайный рецепт',
+        callback_data='random_recipe_inline'
+        )
+    by_ingredients = InlineKeyboardButton(
+        text='🥦 Поиск рецепта по ингредиентам',
+        callback_data='by_ingredients_inline'
+    )
 
     keyboard = InlineKeyboardMarkup(inline_keyboard=[
-        [find_button], [random_button],
+        [find_button, random_button],
+        [by_ingredients],
     ])
     return keyboard
